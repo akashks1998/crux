c77db9587cf5ab0647ca93b0324c0623872056c7
Ashish Kumar <ak.ashish2108@gmail.com>
2019-01-30 00:05:48 +0530

    added cfg3 and inputs

--- lexer_A1/src/lex.py
+++ lexer_A1/src/lex.py
@@ -132,7 +132,8 @@ tokens = [
         'SCHAR',
         'STRING',
         'HASHTAG',
-        'NOTSYM'  
+        'NOTSYM',
+        'QUESMARK'  
 ] + list(keywords.values())
 
 
@@ -202,6 +203,7 @@ t_SCHAR         = r'\'.\''
 t_STRING        = r'\".*\"'
 t_HASHTAG       = r'\#'
 t_NOTSYM        = r'\!'
+t_QUESMARK       = r'\?'
 
 
 # track line no.
@@ -278,7 +280,7 @@ if __name__ == "__main__":
         bold = bold_token.get(tok.value, "")
         if bold != "":
             bold = "; font-weight: bold"
-        color = color_dict.get(str(tok.type), "rgb(200,200,200)")
+        color = color_dict.get(str(tok.type), "rgb(170,170,170)")
         span = "<span style=\"color: " + color + bold + "\">"
         code_out = code_out + " "*(col-cur) + span + tok.value + "</span>"
         cur=col + len(str(tok.value))
