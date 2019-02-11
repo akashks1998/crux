dfdff2d284166f890f55efdf8ba7e68e99d58aff
Ashish Kumar <ak.ashish2108@gmail.com>
2019-02-11 17:38:53 +0530

    basic parser setup

--- parser_A2/parser.py
+++ parser_A2/parser.py
@@ -5,15 +5,47 @@ from lexer import tokens as lexTokens
 
 tokens = lexTokens
 
+start = 'program'
+
 def p_program(p):
     '''program : NUMBER '''
     p[0] = p[1]
 
+
+
+# rule for empty
+def p_empty(p):
+    'empty :'
+    pass
+
+# Error rule for syntax errors
+def p_error(p):
+    print("Syntax error in input!")
+
 if __name__ == "__main__":
     parser = yacc.yacc()
     parser.error = 0
+    
+    if(len(sys.argv) != 3):
+        print("Usage python3 parser.py <debug> <mode>")
+        exit()
+    
+    arglist = sys.argv
+    debug = int(arglist[1])
 
-    # start parsing with lexer from probQlexer
-    p = parser.parse("34",lexer = lexer,debug=1)
-    print(p)
+    if(arglist[2]== "I"):
+        while True:
+            try:
+                s = input('$ > ')
+                if(s=="end"):
+                    break
+            except EOFError:
+                break
+            if not s:
+                continue
+            result = parser.parse(s,lexer = lexer,debug=debug)
+            print(result)
+    else:
+        p = parser.parse("34",lexer = lexer,debug=debug)
+        print(p)
     
