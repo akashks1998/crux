eb6898776cd9821737c60ca63f815d17e92e92ac
Ashish Kumar <ak.ashish2108@gmail.com>
2019-03-11 00:26:10 +0530

    variable declare

--- milestone2/tests/m.cpp
+++ milestone2/tests/m.cpp
@@ -1,12 +1,8 @@
+int main(){
+    int a, b;
+    {
+        int a, b, c;
 
-
-
-
-int **main(int ** m);
-int **main(float** m);
-int **main(int ** m){
-    int a;
-}
-int c(){
-    case 1: int a=0;
+    }
+    int a ;
 }
