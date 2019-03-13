e0680293d49c613e9290c108ada629533b419789
Ashish Kumar <ak.ashish2108@gmail.com>
2019-03-13 22:14:09 +0530

    merge

--- milestone2/tests/m.cpp
+++ milestone2/tests/m.cpp
@@ -1,8 +1,21 @@
-class a{
-    int b;
+class A{
+    int a;
+    char hi(char a){
+        int c = 1;
+        return 0;
+    }
+    int hi(int a){
+        int c = 1;
+        return 0;
+    }
 };
 int main()
 {
-    class a* b;
-    (b).b=5;
+    class A **a,b;
+    // a.a = 1;
+    (*a)->a = 1;
+    int c;
+    a->hi(c);
+
+    return 1;
 }
