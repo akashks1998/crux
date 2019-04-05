f9235282e5b8d4e11756494e4350a52e15de31af
akashks1998 <akashks@iitk.ac.in>
2019-04-05 14:35:36 +0530

    Break corrected

--- milestone3/test/35.cpp
+++ milestone3/test/35.cpp
@@ -3,8 +3,7 @@
 int main()
 {
      int marks[10], i, n, sum = 0, average;
-     while(1 ){
-          i=0; i<n; i++;
+     for(i=0; i<n; i++ ){
           break;
           sum += marks[++i];
      }
